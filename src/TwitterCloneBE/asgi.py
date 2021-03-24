d58653c317cc93d390ce04580ee2e733ec3aaea6
Leonidas <Leonidasmcmillan@gmail.com>
2021-03-24 00:05:48 -0700

    Pre-tweetFun Migrations

--- src/TwitterCloneBE/asgi.py
+++ src/TwitterCloneBE/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for InstacloneBE project.
+ASGI config for TwitterCloneBE project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InstacloneBE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwitterCloneBE.settings')
 
 application = get_asgi_application()
--- src/TwitterCloneBE/wsgi.py
+++ src/TwitterCloneBE/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for InstacloneBE project.
+WSGI config for TwitterCloneBE project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InstacloneBE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwitterCloneBE.settings')
 
 application = get_wsgi_application()
